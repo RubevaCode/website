13a359bb5d8df11ece95a38b631cace7f9f6a3ef
dev-AmitG <dev.amitvg@gmail.com>
2023-09-10 18:36:34 +0530

    fix: mobile touch event fix

--- src/JS/index.js
+++ src/JS/index.js
@@ -6,8 +6,36 @@ window.addEventListener("load", function () {
   AOS.init({
     duration: 1000,
   });
-});
 
-function handleClick(id) {
-  $("html, body").animate({ scrollTop: $(id).offset().top }, 100);
-}
+  const scrollTo = (id) => {
+    $("html, body").animate({ scrollTop: $(id).offset().top }, 100);
+  };
+
+  $("#homeLink").on("click touchstart", function () {
+    scrollTo("#home");
+  });
+
+  $("#aboutLink").on("click touchstart", function () {
+    scrollTo("#advantages");
+  });
+
+  $("#servicesLink").on("click touchstart", function () {
+    scrollTo("#services");
+  });
+
+  $("#contactLink").on("click touchstart", function () {
+    scrollTo("#contact");
+  });
+
+  $("#advantagesBtn").on("click touchstart", function () {
+    scrollTo("#advantages");
+  });
+
+  $("#servicesBtn").on("click touchstart", function () {
+    scrollTo("#services");
+  });
+
+  $("#contactBtn").on("click touchstart", function () {
+    scrollTo("#contact");
+  });
+});
